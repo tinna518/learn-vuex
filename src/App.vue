c151f3af77529e6ab951b1a0fc51c0aea344af5d
tinnat <2573090063@qq.com>
2020-05-28 11:01:21 +0800

    frist commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名">
+         <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+         <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+    <el-button type="primary" @click="onSubmit">登录</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+  </el-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      form:{
+        name:'',
+        password:''
+      }
+    }
+  },
+  methods: {
+    onSubmit(){
+       this.$axios.post('login',{name:this.form.name,password:this.form.password}).then((res)=>{
+         this.commit('save',res.data.username)
+         console.log('发送成功')
+       })
+    }
+  },
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import store from './store.js'
 
+Vue.use(ElementUI)
+Vue.use(Vuex)
+
+axios.defaults.baseURL = 'http://localhost:8083/'
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
